e2b65e7377aef56ff4e7900cf52c44eaf39c0efa
KOOROSH130190\a.sadegh <ahmad.sadegh@gmail.com>
2024-06-12 22:33:58 +0330

    automapper profile for update customer mapping

--- Vehicle.Application/Mappings/MappingProfile.cs
+++ Vehicle.Application/Mappings/MappingProfile.cs
@@ -8,6 +8,7 @@ using Vehicle.Application.Features.Boat.Queries;
 using Vehicle.Application.Features.Bus.Queries;
 using Vehicle.Application.Features.Cars.Commands;
 using Vehicle.Application.Features.Cars.Queries;
+using Vehicle.Application.Features.Customers.Commands;
 using Vehicle.Domain.Entities.Concrete;
 
 namespace Vehicle.Application.Mappings
@@ -18,6 +19,7 @@ namespace Vehicle.Application.Mappings
         {
             CreateMap<Car, CarVM>().ReverseMap();
             CreateMap<Car, HeadlightsCommand>().ReverseMap();
+            CreateMap<Customer, UpdateCustomerCommand>().ReverseMap();
             CreateMap<Boat, BoatVM>().ReverseMap();
             CreateMap<Bus, BusVM>().ReverseMap();
         }
