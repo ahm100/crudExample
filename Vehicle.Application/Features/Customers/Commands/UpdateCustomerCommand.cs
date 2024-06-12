aaf4dab7077f566cc1cf82b137ca969761587a70
KOOROSH130190\a.sadegh <ahmad.sadegh@gmail.com>
2024-06-12 21:36:48 +0330

    update

---
+++ Vehicle.Application/Features/Customers/Commands/UpdateCustomerCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Vehicle.Application.Features.Customers.Commands
+{
+    public class UpdateCustomerCommand : IRequest<int>
+    {
+        public int Id { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string BankAccountNumber { get; set; }
+    }
+}
---
+++ Vehicle.Application/Features/Customers/Commands/UpdateCustomerHandler.cs
@@ -0,0 +1,69 @@
+﻿using MediatR;
+using FluentValidation;
+//using CrudTest.Application.Common.Interfaces;
+//using CrudTest.Domain.Entities;
+using PhoneNumbers;
+//using CrudTest.Domain.Entities;
+//using CrudTest.Application.Commands;
+using Vehicle.Application.Features.Customers.Commands;
+using Vehicle.Domain.Entities.Concrete;
+using Vehicle.Application.Contracts.Persistence;
+using Microsoft.Extensions.Logging;
+using Vehicle.Application.Features.Cars.Commands;
+using Vehicle.Application.Features.Cars.Queries;
+using Vehicle.Application.Common.Exceptions;
+
+namespace CrudTest.Application.Handlers
+{
+    public class UpdateCustomerHandler : IRequestHandler<CreateCustomerCommand, int>
+    {
+        private readonly ICustomerRepository _context;
+        private readonly IValidator<Customer> _validator;
+        private readonly ILogger<CreateCustomerHandler> _logger;
+
+        public UpdateCustomerHandler(ICustomerRepository context, IValidator<Customer> validator, ILogger<CreateCustomerHandler> logger)
+        {
+            _context = context;
+            _validator = validator;
+            _logger = logger;
+        }
+
+        public async Task<int> Handle(UpdateCustomerCommand request, CancellationToken cancellationToken)
+        {
+            var customer = await _context.GetByIdAsync(request.Id);
+
+            if (customer == null)
+            {
+                throw new NotFoundException(nameof(Customer), request.Id);
+            }
+
+            var phoneUtil = PhoneNumberUtil.GetInstance();
+            var numberProto = phoneUtil.Parse(request.PhoneNumber, "US");
+            var formattedPhoneNumber = phoneUtil.Format(numberProto, PhoneNumberFormat.E164);
+
+            if (!phoneUtil.IsValidNumber(numberProto))
+            {
+                throw new ValidationException("Invalid phone number");
+            }
+
+            if (!await _context.CheckUniquenessBYEmail(request.Email))
+            {
+                throw new ValidationException("Email must be unique");
+            }
+
+           
+            customer.FirstName = request.FirstName;
+            customer.LastName = request.LastName;
+            customer.DateOfBirth = request.DateOfBirth;
+            customer.PhoneNumber = numberProto.NationalNumber.ToString();
+            customer.Email = request.Email;
+            customer.BankAccountNumber = request.BankAccountNumber;
+
+          
+                       
+            var result = await _context.AddAsync(customer);
+            _logger.LogInformation($"{result.Id}");
+            return result.Id;
+        }
+    }
+}
